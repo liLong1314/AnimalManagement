692d6079d565215f2a13d748e85fc7bdb442be3a
lijianlong <lijianlong1314@outlook.com>
2023-05-21 20:18:16 +0800

    ServletContainersInitConfig初始化

---
+++ animalManagement/src/main/java/com/lijian/config/ServletContainersInitConfig.java
@@ -0,0 +1,40 @@
+package com.lijian.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * @Author lijia
+ * @createTime 2023/5/21 20:16
+ */
+public class ServletContainersInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    /**
+     * 乱码处理
+     * 控制台打印，会发现有中文乱码问题。
+     * 解决方案:配置过滤器
+     * @return
+     */
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        return new Filter[]{filter};
+    }
+}
